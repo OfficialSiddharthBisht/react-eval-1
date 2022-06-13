e1255e141a0ff8709acad458085685808df6298d
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-13 21:30:19 +0530

    imported Product Listing and Product form in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import ProductListing from './components/ProductListing';
+import ProductForm from './components/ProductForm';
 
 function App() {
   return (
     <div className="App">
-     
+     <ProductForm />
+     <ProductListing />
     </div>
   );
 }
