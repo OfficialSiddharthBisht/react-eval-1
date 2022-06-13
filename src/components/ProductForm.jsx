744dca22432b4d777a7946a2eb7aa906fed1a19b
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-13 21:28:53 +0530

    product form started

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -4,8 +4,19 @@ function ProductForm(){
     return (
         <div>
         <h1>Product Form</h1>
+        <form>
+            <label>
+                Title : <input type="text" name="title" />
+                Gender : <input type= "text" name='gender' />
+                Category : <input type= "text" name='category' />
+                Price : <input type= "number" name='price' />
+                Image : <input type= "file" name = "image"/>
+            </label>
+        </form>
         </div>
     );
 }
 
 export default ProductForm;
+
+// title gender category price image
